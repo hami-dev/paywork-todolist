5bfc10777a07f4abc721328fe83b065e35265769
hami-dev <rinrin.xml@gmail.com>
2021-09-02 06:00:30 +0900

    Refactor: api url 수정

--- src/Utils/Api.ts
+++ src/Utils/Api.ts
@@ -1,8 +1,9 @@
 const BASE_URL = 'http://dummy-server.io';
+const URL = BASE_URL + ':4000/todo';
 
 // server의 data를 받아옴
 export const fetchTodos: any = () => {
-  return fetch(BASE_URL + ':4000/todo').then((res) => {
+  return fetch(URL).then((res) => {
     if (!res.ok) {
       console.log(res.statusText); //error
     }
